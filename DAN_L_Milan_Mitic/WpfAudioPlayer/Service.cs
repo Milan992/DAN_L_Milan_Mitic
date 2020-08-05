a2d7e1c968d702419c1f9c4a0a9e9e09993925e9
Milan992 <mitic.gsm@gmail.com>
2020-08-05 10:08:58 +0200

    LogIn window and ViewModel created

---
+++ DAN_L_Milan_Mitic/WpfAudioPlayer/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAudioPlayer
+{
+    class Service
+    {
+        internal void AddUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool IsUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
