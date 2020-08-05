f599f893fe831a9dd8b9e0456152fd32ea41eaa9
Milan992 <mitic.gsm@gmail.com>
2020-08-05 10:25:43 +0200

    Imported Entity Framework

--- AudioPlayerDataBaseScript.sql
+++ AudioPlayerDataBaseScript.sql
@@ -3,11 +3,11 @@ CREATE DATABASE AudioPlayer
 GO
 USE AudioPlayer;
 
-if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblUser')
-drop table tblUser;
-
 if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblSong')
 drop table tblSong;
+
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblUser')
+drop table tblUser;
 
 if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblPlayedSong')
 drop table tblPlayedSong;
@@ -21,6 +21,7 @@ Pass varchar(30) check(len(Pass) > 6) not null
 
 create table tblSong(
 SongID int identity(1,1) primary key,
+UserID int foreign key (UserID) references tblUser(UserID),
 SongName varchar(30) not null,
 Author varchar(30),
 DurationInSeconds int not null 
--- DAN_L_Milan_Mitic/WpfAudioPlayer/ViewModels/UserViewModel.cs
+++ DAN_L_Milan_Mitic/WpfAudioPlayer/ViewModels/UserViewModel.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfAudioPlayer.Views;
 
 namespace WpfAudioPlayer.ViewModels
 {
     class UserViewModel
     {
+        User user;
+        Service service = new Service();
+
+        #region Constructors
+
+        public UserViewModel(User userOpen)
+        {
+            user = userOpen;
+        }
+
+        public UserViewModel(User userOpen, string userName)
+        {
+            user = userOpen;
+            songsList = service.GetUserSongs(userName);
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
     }
 }
